17ee459465dd38bd6d7e0a52c08b242fae3245a6
William Qian <wqian94@mit.edu>
2016-05-20 18:40:14 -0400

    Added PHP server-side script and some documentation in README for it.

--- receive.php
+++ receive.php
@@ -1,46 +1,3 @@
-AuO version 0.1 (beta)
-===
-AuO, a browser-based audio recording and editing application. Uses browser-native technologies to
-avoid third-party dependencies.
-
-# Using AuO
-
-Include AuO in any application by including AuO.js. Then, create an instance with
-
-```javascript
-const auo = new AuO(link_to_server_url);
-```
-
-where link_to_server is the URL to upload audio clips. Ignore this parameter to disable online
-saving.
-
-To launch AuO, simply call
-
-```javascript
-auo.launch();
-```
-
-and the interface should appear. To suspend AuO, click Close or call
-
-```javascript
-auo.suspend();
-```
-
-to make the interface disappear and prevent resource use. When you are ready to use AuO once more,
-call
-
-```javascript
-auo.launch();
-```
-
-again to relaunch the interface using the same instance.
-
-# Providing a backend for AuO
-
-The code in receive.php provides a simple PHP server-side script for supporting online file uploads
-from AuO. This code is reproduced below:
-
-```php
 <?php
 /**
  * receive.php
@@ -78,4 +35,3 @@ $link = "$HOSTNAME$link/sounds/$filename";
 
 echo $link;
 ?>
-```
